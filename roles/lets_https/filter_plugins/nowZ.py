e5f27ee027f13ba94f4aa1a155aecfa020b445a7
mz0 <marcuzero@gmail.com>
2019-05-14 08:31:43 +0300

    w.i.p.

--- roles/lets_https/filter_plugins/nowZ.py
+++ roles/lets_https/filter_plugins/nowZ.py
@@ -1,8 +1,13 @@
 from datetime import datetime
+from time import time
 
 def nowZ(arg):
     return datetime.utcnow().strftime('%Y%m%d%H%M%SZ')
 
+def nowS(arg):
+    return int(time())
+
 class FilterModule(object):
     def filters(self):
-        return { 'nowZ': nowZ }
+        return { 'nowZ': nowZ,
+                 'nowS': nowS }
